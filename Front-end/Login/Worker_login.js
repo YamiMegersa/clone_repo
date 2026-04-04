e9d8c8317465882866f89c5574c6f118aae5fee0
ZFakir <zaeem.zf.fakir@gmail.com>
2026-04-04 15:48:46 +0200

    Municipal skeleton code added

---
+++ Front-end/Login/Worker_login.js
@@ -0,0 +1,41 @@
+const loginForm = document.getElementById('MunicipalLoginForm');
+const IDInput = document.getElementById('municipalID');
+const passwordInput = document.getElementById('password');
+const errorMessage=document.getElementById('error');
+
+loginForm.addEventListener('submit', async function(event) {
+    // No refresh
+    event.preventDefault();
+
+    // 4. Get the values from the inputs
+    const username = IDInput.value.trim();//trim removes spaces from end
+    const password = passwordInput.value;
+    if((!username||!password)){
+        errorMessage.textContent="Please your enter your Municipal ID and password";
+        errorMessage.classList.remove('hidden');
+        return;
+    }
+    // 5. Do something with the data
+    try {
+        // This is where you send the ID and Password to your server
+        // which then talks to Google's API
+        const response = await fetch('</api/login>', {//Change once db runs
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            //displayMessage("Success! Redirecting...", "success");
+            window.location.href = "/<ResidentPage>";
+        } else {
+            errorMessage.textContent="Incorrect username or password";
+            errorMessage.classList.remove('hidden');
+        }
+    } catch (err) {
+        errorMessage.textContent = "Something has gone terribly wrong.";
+        errorMessage.classList.remove('hidden');
+    }   
+});
